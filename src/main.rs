5cca3009b9a7d5ad19e9a2b642e93037a277729d
ZioCecio <gabriecetro@libero.it>
2023-11-30 16:54:52 +0100

    Initialize threads reading a config file

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,66 @@
+use serde::{Deserialize, Serialize};
+use serde_yaml;
+use std::collections::HashMap;
+use std::fs;
 use std::io;
+use std::sync::Arc;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::broadcast;
+use tokio::sync::Mutex;
 
 extern crate tokio;
 
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+struct ProxyConfig {
+    pub services: Vec<ServiceInfo>,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+struct ServiceInfo {
+    pub service_name: String,
+    pub from: String,
+    pub to: String,
+}
+
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let from = "127.0.0.1:3000";
-    let to = "127.0.0.1:8000";
+    let file_content = fs::read_to_string("config.yml")?;
 
+    let proxy_config: ProxyConfig = serde_yaml::from_str(&file_content).unwrap();
+
+    let mut tasks = Vec::new();
+    let services_rules_buffer: Arc<Mutex<HashMap<String, Arc<Mutex<Vec<&[u8]>>>>>> =
+        Arc::new(Mutex::new(HashMap::new()));
+
+    for service in proxy_config.services {
+        let rules_buffer: Arc<Mutex<Vec<&[u8]>>> = Arc::new(Mutex::new(Vec::new()));
+        services_rules_buffer
+            .lock()
+            .await
+            .insert(service.service_name.clone(), rules_buffer.clone());
+
+        tasks.push(start_service(service, rules_buffer.clone()));
+    }
+
+    futures::future::join_all(tasks).await;
+    Ok(())
+}
+
+async fn start_service(service: ServiceInfo, rules_buffer: Arc<Mutex<Vec<&[u8]>>>) -> io::Result<()> {
+    let from = &service.from;
     let listener = TcpListener::bind(from).await?;
+    println!("Started service {}", service.service_name);
 
     loop {
+        let to = service.to.clone();
         let (mut socket, addr) = listener.accept().await?;
+
+        let rules_buffer = rules_buffer.clone();
         tokio::spawn(async move {
             println!("New connection from {:?}!", addr);
 
-            let mut remote = match TcpStream::connect(to).await {
+            let mut remote = match TcpStream::connect(&to).await {
                 Ok(socket) => socket,
                 Err(e) => {
                     eprintln!("Error estabilishing connection to the server: {e}");
@@ -75,17 +118,23 @@ where
         }
 
         if is_client {
-            let request = std::str::from_utf8(&buf[0..bytes_read]).unwrap();
-            if request.contains(".git") {
+            //for rule in rules
+
+            if find_subsequence(&buf[0..bytes_read], b".git").is_some() {
                 return Ok(0);
             }
             // Other filters...
-
-            println!("{:?}", request);
         }
+
         write.write_all(&buf[0..bytes_read]).await?;
         copied += bytes_read;
     }
 
     Ok(copied)
+}
+
+fn find_subsequence(haystack: &[u8], needle: &[u8]) -> Option<usize> {
+    haystack
+        .windows(needle.len())
+        .position(|window| window == needle)
 }
